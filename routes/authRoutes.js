283ce83f06260f4baba454485d7c0eaacd559a1d
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2021-07-03 20:47:29 +0530

    c

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -17,7 +17,7 @@ router.get(
   function (req, res) {
     // console.log(req.user, req.isAuthenticated());
     req.session.save(function (err) {
-      res.redirect("/explore-events");
+      res.redirect("/user/home");
     });
   }
 );
