f10893a1a03e54497a5b266e5f2aa4ea98ca7423
Dinesh Shah <dineshshah8234@gmail.com>
2021-07-08 00:31:08 +0530

    C

--- controllers/authController.js
+++ controllers/authController.js
@@ -10,42 +10,27 @@ const AppError = require("../utils/appError.js");
 const catchAsync = require("../utils/catchAsync");
 
 // this function will return you jwt token
-const signToken = (id) =>
-  jwt.sign({ id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+const signToken = (id) => jwt.sign({ id }, process.env.JWT_SECRET);
 
 const signTokenForSalesLogin = (salesPersonId) =>
-  jwt.sign({ salesPersonId }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  jwt.sign({ salesPersonId }, process.env.JWT_SECRET);
 
 const signTokenForCommunityLogin = (userId, communityId) =>
   jwt.sign(
     { userId: userId, communityId: communityId },
-    process.env.JWT_SECRET,
-    {
-      expiresIn: process.env.JWT_EXPIRES_IN,
-    }
+    process.env.JWT_SECRET
   );
 
 // this function use signToken function for creating and sending token
 const createSendToken = (user, statusCode, req, res) => {
   const token = signToken(user._id);
-  const cookieOptions = {
-    expires: new Date(
-      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
-    ),
-    httpOnly: true,
-  };
-  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
-  res.cookie("jwt", token, cookieOptions);
 
   //remove password from output
   user.password = undefined;
 
   res.status(statusCode).json({
     status: "success",
+
     token,
     data: {
       user,
