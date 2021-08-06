f74c1e9b082f54d916ef328697bf27c560b548f9
Dinesh Shah <dineshshah8234@gmail.com>
2021-08-07 02:58:50 +0530

    c

--- client/src/reducers/authSlice.js
+++ client/src/reducers/authSlice.js
@@ -6,42 +6,24 @@ const authSlice = createSlice({
   initialState: {
     isSignedIn: false,
     token: "",
-    
-    error:false
+
+    error: false,
   },
 
   reducers: {
-
-   
-
-    
-    ResetError(state,action){
-  
-     state.error = false;
-     
-  },
-    hasError(state,action){
-  
-     state.error = action.payload;
-     
-  },
+    ResetError(state, action) {
+      state.error = false;
+    },
+    hasError(state, action) {
+      state.error = action.payload;
+    },
     SignIn(state, action) {
       state.token = action.payload.token;
       state.isSignedIn = action.payload.isSignedIn;
-      
-     
-   
     },
     SignOut(state, action) {
-
-      (async() => {
-       await window.localStorage.clear();
-      })();
       state.token = null;
       state.isSignedIn = false;
-       
-      
-
     },
   },
 });
