e4935b46ef80f42ea9dbd0e431bd2ab7247abe06
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2021-08-11 20:06:38 +0530

    commit

--- client/src/components/Dashboard/EditEvent/Speakers.js
+++ client/src/components/Dashboard/EditEvent/Speakers.js
@@ -17,7 +17,7 @@ import SearchIcon from "@material-ui/icons/Search";
 import AddNewSpeaker from "./FormComponents/EditSpeakersForms/AddNewSpeaker";
 import { useParams } from "react-router";
 import { useEffect } from "react";
-import { editEvent, errorTrackerForFetchSpeakers, fetchEvent, fetchSessions, fetchSpeakers } from "../../../actions";
+import { errorTrackerForFetchSpeakers, fetchEvent, fetchSessions, fetchSpeakers } from "../../../actions";
 import { useDispatch, useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import Loader from "../../Loader";
