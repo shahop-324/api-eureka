8255421361e44afceaf34b5bee238d473a6a4570
Dinesh Shah <dineshshah8234@gmail.com>
2021-08-12 10:02:22 +0530

    Merge branch 'main' of https://github.com/shahop-324/api-eureka into main

--- client/src/reducers/authSlice.js
+++ client/src/reducers/authSlice.js
@@ -7,12 +7,13 @@ const authSlice = createSlice({
     isSignedIn: false,
     token: "",
     error: false,
+    isSignedInThroughGoogle: false,
   },
 
   reducers: {
     ResetError(state, action) {
       state.error = false;
-     // state.isSignedIn = false;
+      // state.isSignedIn = false;
     },
     hasError(state, action) {
       state.error = action.payload;
@@ -21,16 +22,19 @@ const authSlice = createSlice({
     disabledError(state, action) {
       state.error = false;
       state.isSignedIn = false;
-      
     },
     SignIn(state, action) {
       state.token = action.payload.token;
-      state.isSignedIn = true
-      
+      state.isSignedIn = true;
+
+      if (action.payload.isSignedThroughGoogle) {
+        state.isSignedInThroughGoogle = action.payload.isSignedThroughGoogle;
+      }
     },
     SignOut(state, action) {
       state.token = null;
       state.isSignedIn = false;
+      state.isSignedInThroughGoogle = false;
     },
   },
 });
