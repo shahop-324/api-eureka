1ab90b7883605157067d1b9b88a629c0ef1e2e2f
dinesh <dinesh.11903737@lpu.co.in>
2021-09-08 11:25:50 +0530

    successfully autorized mailchimp

--- app.js
+++ app.js
@@ -69,8 +69,13 @@ const urlToGetUserEmail =
 const MAILCHIMP_CLIENT_ID = "919814706970";
 const MAILCHIMP_CLIENT_SECRET =
   "3837302297576b7845b5ced8bd4691bb69ac7b8c5f90645887";
-const BASE_URL = "http://127.0.0.1:3001";
-const OAUTH_CALLBACK = `${BASE_URL}/signin`;
+
+console.log(process.env.NODE_ENV, "klljlkl;kmfghytredswrtyuiop");
+const BASE_URL =
+  process.env.NODE_ENV === "development"
+    ? "http://127.0.0.1:3000"
+    : "https://www.evenz.co.in";
+const OAUTH_CALLBACK = `${BASE_URL}/api-eureka/eureka/v1/oauth/mailchimp/callback`;
 
 app.use(
   cors({
@@ -250,7 +255,7 @@ app.get("/api-eureka/eureka/v1/current_user", (req, res) => {
 
 // 2. The login link above will direct the user here, which will redirect
 // to Mailchimp's OAuth login page.
-app.get("/api-eureka/auth/mailchimp", (req, res) => {
+app.get("/api-eureka/eureka/v1/auth/mailchimp", (req, res) => {
   res.redirect(
     `https://login.mailchimp.com/oauth2/authorize?${querystring.stringify({
       response_type: "code",
@@ -263,7 +268,7 @@ app.get("/api-eureka/auth/mailchimp", (req, res) => {
 // 3. Once // 3. Once the user authorizes your app, Mailchimp will redirect the user to
 // this endpoint, along with a code you can use to exchange for the user's
 // access token.
-app.get("/api-eureka/oauth/mailchimp/callback", (req, res) => {
+app.get("/api-eureka/eureka/v1/oauth/mailchimp/callback", (req, res) => {
   // Here we're exchanging the temporary code for the user's access token.
   //console.log(req.body.code);
   console.log(req.query.code);
@@ -316,68 +321,6 @@ app.get("/api-eureka/oauth/mailchimp/callback", (req, res) => {
   `);
         });
     });
-
-  // const tokenResponse = await fetch(
-  //   "https://login.mailchimp.com/oauth2/token",
-  //   {
-  //     method: "POST",
-  //     body: new URLSearchParams({
-  //       grant_type: "authorization_code",
-  //       client_id: MAILCHIMP_CLIENT_ID,
-  //       client_secret: MAILCHIMP_CLIENT_SECRET,
-  //       redirect_uri: OAUTH_CALLBACK,
-  //       code: req.query.code,
-  //     }),
-  //   }
-  // );
-
-  // const { accessToken } = await tokenResponse.json();
-  // console.log(accessToken);
-
-  // Now we're using the access token to get information about the user.
-  // Specifically, we want to get the user's server prefix, which we'll use to
-  // make calls to the API on their behalf.  This prefix will change from user
-  // to user.
-  // const metadataResponse = await fetch(
-  //   "https://login.mailchimp.com/oauth2/metadata",
-  //   {
-  //     headers: {
-  //       Authorization: `OAuth ${accessToken}`,
-  //     },
-  //   }
-  // );
-
-  // const { dc } = await metadataResponse.json();
-  // console.log(dc);
-
-  // Below, we're using the access token and server prefix to make an
-  // authenticated request on behalf of the user who just granted OAuth access.
-  // You wouldn't keep this in your production code, but it's here to
-  // demonstrate how the call is made.
-
-  // mailchimp.setConfig({
-  //   accessToken: accessToken,
-  //   server: dc,
-  // });
-
-  // const response = await mailchimp.ping.get();
-  // console.log(response);
-
-  // res.send(`
-  //   <p>This user's access token is ${accessToken} and their server prefix is ${dc}.</p>
-
-  //   <p>When pinging the Mailchimp Marketing API's ping endpoint, the server responded:<p>
-
-  //   <code>${response}</code>
-  // `);
-
-  // In reality, you'd want to store the access token and server prefix
-  // somewhere in your application.
-  // fakeDB.getCurrentUser();
-  // fakeDB.storeMailchimpCredsForUser(user, {
-  //   dc,
-  //   accessToken
-  // });
 });
 
 app.get("/api-eureka/eureka/v1/logout", (req, res) => {
--- client/src/components/Dashboard/Integrations/Mailchimp.js
+++ client/src/components/Dashboard/Integrations/Mailchimp.js
@@ -3,7 +3,10 @@ import React from "react";
 import "./Styles/IntegrationCard.scss";
 
 import { makeStyles } from "@material-ui/core/styles";
-
+const { REACT_APP_MY_ENV } = process.env;
+const BaseURL = REACT_APP_MY_ENV
+  ? "http://localhost:3000/api-eureka/eureka/v1"
+  : "https://www.evenz.co.in/api-eureka/eureka/v1";
 const useStyles = makeStyles((theme) => ({
   root: {
     display: "flex",
@@ -32,9 +35,7 @@ const Mailchimp = () => {
           style={{ display: "grid", gridTemplateColumns: "0.7fr 8fr 0.7fr" }}
         >
           <Avatar
-            src={
-              "https://www.drupal.org/files/project-images/MC_Logo.jpg"
-            }
+            src={"https://www.drupal.org/files/project-images/MC_Logo.jpg"}
             alt={"Mailchimp"}
             className={classes.large}
             variant="rounded"
@@ -43,13 +44,20 @@ const Mailchimp = () => {
           <div>
             <div className="integration-name mb-2">Mailchimp</div>
             <div className="integration-short-description">
-              Sync all of your attendee, leads, interested people and more email groups to your mailchimp account.
+              Sync all of your attendee, leads, interested people and more email
+              groups to your mailchimp account.
             </div>
           </div>
 
           <div style={{ justifySelf: "end" }}>
             <button className="btn btn-outline-primary btn-outline-text">
-              Add
+              <a
+                href={`${BaseURL}/auth/mailchimp`}
+                style={{ textDecoration: "none" }}
+              >
+                {" "}
+                Add
+              </a>
             </button>
           </div>
         </div>
