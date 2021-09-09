e352d7fa1357783824cef0bd3e49fc4a198a60b4
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2021-09-09 23:59:37 +0530

    commit

---
+++ client/src/components/MailchimpConnect.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const MailchimpConnect = () => {
+  return (
+    <>
+      <div
+        className="d-flex flex-row align-items-center justify-content-center"
+        style={{ height: "100vh" }}
+      >
+        <img
+          src={"https://www.drupal.org/files/project-images/MC_Logo.jpg"}
+          alt="mailchimp connect"
+          style={{ maxHeight: "320px", borderRadius: "10px" }}
+        />
+      </div>
+    </>
+  );
+};
+
+export default MailchimpConnect;
