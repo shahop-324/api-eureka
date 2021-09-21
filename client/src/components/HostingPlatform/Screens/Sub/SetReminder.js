f3a1dac317081665c9b4efb64aed6529a3266cd3
SHREYANSH SHAH <60038350+shahop-324@users.noreply.github.com>
2021-09-21 07:30:48 +0530

    m

--- client/src/components/HostingPlatform/Screens/Sub/SetReminder.js
+++ client/src/components/HostingPlatform/Screens/Sub/SetReminder.js
@@ -1,16 +1,40 @@
 import { Dialog } from "@material-ui/core";
-
 import styled from "styled-components";
-
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { useTheme } from "@material-ui/core/styles";
 
+import IconButton from "@material-ui/core/IconButton";
+import CancelRoundedIcon from "@mui/icons-material/CancelRounded";
+
 import React from "react";
+
+const WidgetHeadlineWithClose = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+
+  font-size: 1rem;
+  font-weight: 500;
+  font-family: "Ubuntu";
+  color: #152d35;
+
+  border-bottom: 1px solid #152d35;
+`;
 
 const SetReminderPaper = styled.div`
   background-color: #ffffff;
   height: 480px;
   width: 400px;
+`;
+
+const FormLabel = styled.div`
+  font-weight: 500;
+  font-size: 0.85rem;
+  font-family: "Ubuntu";
+  color: #152d35;
+
+  margin-bottom: 6px;
 `;
 
 const SetReminder = ({ open, handleClose }) => {
@@ -25,7 +49,29 @@ const SetReminder = ({ open, handleClose }) => {
         open={open}
         aria-labelledby="responsive-dialog-title"
       >
-        <SetReminderPaper></SetReminderPaper>
+        <SetReminderPaper className="px-4 py-3">
+        <WidgetHeadlineWithClose className="pb-1 mb-4">
+            <div>Set Reminder</div>
+            <div style={{ textAlign: "end" }}>
+              <IconButton
+                onClick={() => {
+                  handleClose();
+                }}
+              >
+                <CancelRoundedIcon />
+              </IconButton>
+            </div>
+          </WidgetHeadlineWithClose>
+
+          <FormLabel>Personal note</FormLabel>
+
+          <textarea className="form-control" ></textarea>
+
+          <FormLabel className="">Personal note</FormLabel>
+
+
+
+        </SetReminderPaper>
       </Dialog>
     </>
   );
