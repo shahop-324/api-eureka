27580780bc1bc5410be58b80cc108ab0a298c5a0
dinesh <dinesh.11903737@lpu.co.in>
2021-09-26 15:27:43 +0530

    m

--- client/src/components/Dashboard/IntegrationSubComponents.js/StreamDestinations/StreamDestination.js
+++ client/src/components/Dashboard/IntegrationSubComponents.js/StreamDestinations/StreamDestination.js
@@ -1,13 +1,51 @@
-import React from 'react';
+import React from "react";
+import { useEffect } from "react";
+
+const loadFbSDK = () => {
+  return new Promise((resolve) => {
+    const script = document.createElement("script");
+    script.src =
+      "https://connect.facebook.net/en_GB/sdk.js#xfbml=1&version=v12.0&appId=878344546151410&autoLogAppEvents=1";
+    script.nonce = "kh8XDXHL";
+    script.async = true;
+    script.defer = true;
+    script.crossOrigin = "anonymous";
+    script.onload = () => {
+      resolve(true);
+    };
+    script.onerror = () => {
+      resolve(false);
+    };
+    document.body.appendChild(script);
+  });
+};
 
 const StreamDestination = () => {
-    return (
-        <>
+  useEffect(() => {
+    const res = async () => {
+      const x = await loadFbSDK();
 
+      if (!x) {
+        console.log("Failed to connect to fb are you online?");
+      }
+    };
 
-
-        </>
-    )
-}
+    res();
+  }, []);
+  return (
+    <>
+      <div id="fb-root"></div>
+      <div
+        class="fb-login-button"
+        data-width=""
+        data-size=""
+        data-button-type=""
+        data-layout=""
+        data-auto-logout-link=""
+        data-use-continue-as=""
+      ></div>
+    </>
+  );
+};
 
 export default StreamDestination;
